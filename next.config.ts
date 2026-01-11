abc7ef8e71db1e77ba969beaf22300e8278c8da5
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2026-01-11 14:47:53 +0700

    Show both draft edit order & invoice failed

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,6 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_API_URL:
       process.env.NEXT_PUBLIC_API_URL || "http://localhost:3060/api",
   },
-  reactStrictMode: false,
 };
 
 export default nextConfig;
