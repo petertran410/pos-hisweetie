73632ae860f822d460eafb8e725bc4055d7bab50
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2026-02-13 16:56:12 +0700

    Error

--- components/customers/CustomerDetailRow.tsx
+++ components/customers/CustomerDetailRow.tsx
@@ -77,7 +77,7 @@ export function CustomerDetailRow({
   return (
     <tr>
       <td colSpan={colSpan} className="py-2 bg-gray-50">
-        <div className="bg-white rounded-lg border border-gray-200 overflow-hidden sm:max-w-[640px] md:max-w-[768px] lg:max-w-[830px] xl:max-w-[1135px] 2xl:max-w-[1600px]">
+        <div className="bg-white rounded-lg border border-gray-200 overflow-hidden sm:max-w-[640px] md:max-w-[768px] lg:max-w-[830px] xl:max-w-[1135px] 2xl:max-w-[1545px]">
           <div className="p-6">
             <div className="border-b border-gray-200 pb-4 mb-6 ">
               <div className="flex items-center justify-between">
@@ -189,8 +189,8 @@ export function CustomerDetailRow({
                           {customer.gender === true
                             ? "Nam"
                             : customer.gender === false
-                            ? "Nữ"
-                            : "Chưa có"}
+                              ? "Nữ"
+                              : "Chưa có"}
                         </div>
                       </div>
 
