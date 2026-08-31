a93eba7379737b5ddd26b6e1601f85d72b3fd33d
toandieptra <90399762+toandieptra@users.noreply.github.com>
2026-08-31 11:31:03 +0700

    chore: allow dev origin 192.168.1.234

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["192.168.1.234"],
   env: {
     NEXT_PUBLIC_API_URL:
       process.env.NEXT_PUBLIC_API_URL || "http://localhost:3060/api",
